9546fe07bf05c62abb5df3a6f972da12bda6910b
Lexie Crawford <lexcraw4d@gmail.com>
2021-05-26 02:35:02 -0500

    added correct prompts and prompt functions work

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const inquirer = require('inquirer');
-const { type } = require('os');
 const Employee = require('./lib/Employee');
-
-
-const initializeQuestions = () => {
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+// let gitHub = github();
+const initializePrompt = () => {
+	inquirer
+		.prompt([
+			{
+				type: 'checkbox',
+				name: 'employeeType',
+				message: 'Which team of employee would you like to add?',
+				choices: ['Engineer', 'Intern', 'Manager', 'None to add, my team is complete!'],
+			},
+		])
+		.then((response) => {
+			if (response.employeeType != 'None to add, my team is complete!') {
+				initializeQuestions(response.employeeType);
+				console.log(response);
+			} else {
+				//write html file here
+			}
+		});
+};
+const initializeQuestions = (employeeRole) => {
+	console.log('<------ Adding new employee ------>');
 	inquirer
 		.prompt([
 			{
@@ -15,11 +35,7 @@ const initializeQuestions = () => {
 				type: 'number',
 				name: 'id',
 				message: 'What is your id?',
-				validate: (number) => {
-					if (typeof number != 'number') {
-						return 'Please enter a valid number.';
-					}
-				},
+				//validate later here
 			},
 			{
 				type: 'text',
@@ -27,10 +43,28 @@ const initializeQuestions = () => {
 				message: 'What is your email address?',
 			},
 		])
-
-		.then((response) => {
-			console.table(response);
-			// console.log(new Employee(response.name, response.id, response.email));
+		.then((allInfo) => {
+			if (employeeRole === 'Engineer') {
+				console.log('Role to be added: Engineer');
+				//create Engineer inquirer prompt
+				//use allinfo.name allinfo.id etc for initial info
+				//inquire prompt additional question for param
+				// and add here class fx e.g. Engineer(passInfo)
+			} else if (employeeRole === 'Manager') {
+				console.log('Role to be added: Manager');
+				//mgr prompt inquirer variable here
+				// and add here class fx e.g. Manage(passInfo)
+			} else if (employeeRole == 'Intern') {
+				console.log('Role to be added: Intern');
+				//intern prompt inquirer variable here
+				// and add here class fx e.g. intern(passInfo)
+			}
 		});
 };
-initializeQuestions();
+
+//get prompts for roles
+//add information and pass as params to class
+//.then after else if
+//push as an obj to [empty arr] var after fx class
+
+initializePrompt();
